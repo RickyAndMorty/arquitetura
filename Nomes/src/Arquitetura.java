6721c41f919d5e90f2c6f38c0ff7b8404d8d0e25
ricardo <ricardo.santos.0492@gmail.com>
2018-08-12 10:43:13 -0300

    commit F

--- Nomes/src/Arquitetura.java
+++ Nomes/src/Arquitetura.java
@@ -4,6 +4,23 @@ import java.io.InputStreamReader;
 import static java.lang.System.in;
 import java.util.Scanner; 
 
+/*
+Para essa atividade, você deverá criar aplicativo desktop que armazene em um array/lista um nome 
+informado pelo usuario e permita listar todos os nomes armazenados. Cabe ao usuário do sistema 
+decidir quando quer informar um novo nome ou listar os nomes armazenados. O código fonte da 
+aplicação deverá ser armazenado em um repositório na sua conta no GitHub. Se você usar alguma IDE 
+(e.g., Netbeans, IntelliJ, etc), submeta o projeto criado pela IDE, contendo todas as pastas e arquivos, 
+com exceção dos arquivos executáveis. Para esta atividade, você deve submeter no Moodle o link do 
+repositório no GitHub. O professor irá analisar o código diretamente no GitHub. 
+
+Adicionalmente, considere as seguintes observações:
+
+Excepcionalmente, essa atividade é individual;
+É necessário criar uma interface para interação com o usuário, mas não precisa ser uma 
+interface gráfica, pode ser textual;
+Os nomes armazenados no array/lista não precisam ser gravados em banco de dados;
+
+*/
 public class Arquitetura 
 {
  
