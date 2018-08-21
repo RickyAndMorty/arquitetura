d2b902984deb0b64d8768a709206f71b21a3b7fa
ViniciusLavorte <Viniciuspereira@alunos.utfpr.edu.br>
2018-08-21 00:04:38 -0300

    Adicionando classe atualidade

---
+++ ListaClientes/src/listaclientes/Nacionalidade.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listaclientes;
+
+/**
+ *
+ * @author vinicius
+ */
+public class Nacionalidade {
+    public String Pais;
+    
+    public Nacionalidade(String Pais){
+        this.Pais = Pais;
+    }
+
+    public String getPais() {
+        return Pais;
+    }
+
+    public void setPais(String Pais) {
+        this.Pais = Pais;
+    }
+    
+}
